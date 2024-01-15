2d54dc2b75bb61bba2672d8e296f8fa5280d7ee4
Sahil Golder <124379496+Sahil-047@users.noreply.github.com>
2024-01-15 21:38:09 +0530

    commit

---
+++ script.js
@@ -0,0 +1,215 @@
+function colorwhite() {
+    document.body.style.backgroundColor = "white";
+    let elem1 = document.getElementById('hs');
+    elem1.classList = []
+    elem1.classList.add('hs-0');
+    let elem2 = document.getElementById('gr');
+    elem2.classList = []
+    elem2.classList.add('hs-0');
+    let elem3 = document.getElementById('cr');
+    elem3.classList = []
+    elem3.classList.add('hs-0');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList = []
+    elem4.classList.add('navelements', 'nc-0');
+    let elem5 = document.getElementById('txt');
+    elem5.classList = []
+    elem5.classList.add('txa');
+    let elem6 = document.getElementById('p5');
+    elem6.classList = []
+    elem6.classList.add('txa');
+    let elem7 = document.getElementById('ps');
+    elem7.classList = []
+    elem7.classList.add('txa');
+    let elem8 = document.getElementById('co');
+    elem8.classList = []
+    elem8.classList.add('txa');
+    let elem9 = document.getElementById('ct');
+    elem9.classList = []
+    elem9.classList.add('txa');
+    let elem10 = document.getElementById('sch');
+    elem10.classList = []
+    elem10.classList.add('searchbar', 'scb');
+    let elem11 = document.getElementById('bt1');
+    elem11.classList = []
+    elem11.classList.add('about');
+    let elem12 = document.getElementById('bt2');
+    elem12.classList = []
+    elem12.classList.add('contact');
+    let elem13 = document.getElementById('join');
+    elem13.classList = []
+    elem13.classList.add('logo');
+    let elem14 = document.getElementById('felm');
+    elem14.classList = []
+    elem14.classList.add('fc');
+
+
+
+}
+function colorpurple() {
+
+
+
+    document.body.style.backgroundColor = "#7049c3";
+    let elem1 = document.getElementById('hs');
+    elem1.classList = []
+    elem1.classList.add('hs-1');
+    let elem2 = document.getElementById('gr');
+    elem2.classList = []
+    elem2.classList.add('hs-1');
+    let elem3 = document.getElementById('cr');
+    elem3.classList = []
+    elem3.classList.add('hs-1');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList = []
+    elem4.classList.add('navelements', 'nc-1');
+    let elem5 = document.getElementById('txt');
+    elem5.classList = []
+    elem5.classList.add('txc');
+    let elem6 = document.getElementById('p5');
+    elem6.classList = []
+    elem6.classList.add('txc');
+    let elem7 = document.getElementById('ps');
+    elem7.classList = []
+    elem7.classList.add('txc');
+    let elem8 = document.getElementById('co');
+    elem8.classList = []
+    elem8.classList.add('txc');
+    let elem9 = document.getElementById('ct');
+    elem9.classList = []
+    elem9.classList.add('txc');
+    let elem10 = document.getElementById('sch');
+    elem10.classList = []
+    elem10.classList.add('searchbar', 'scb');
+    let elem11 = document.getElementById('bt1');
+    elem11.classList = []
+    elem11.classList.add('about', 'nc-1');
+    let elem12 = document.getElementById('bt2');
+    elem12.classList = []
+    elem12.classList.add('contact', 'bn2');
+    let elem13 = document.getElementById('join');
+    elem13.classList = []
+    elem13.classList.add('logo', 'nc-1');
+    let elem14 = document.getElementById('felm');
+    elem14.classList = []
+    elem14.classList.add('fc');
+
+
+    // elem1.classList.add('hs-1');
+    // elem1.classList.remove('hs-0', 'hs-2', 'hs-3');
+    // let elem2 = document.getElementById('gr');
+    // elem2.classList.add('hs-1');
+    // elem2.classList.remove('hs-0', 'hs-2', 'hs-3');
+    // let elem3 = document.getElementById('cr');
+    // elem3.classList.add('hs-1');
+    // elem3.classList.remove('hs-0', 'hs-2', 'hs-3');
+    // let elem4 = document.getElementById('nav1');
+    // elem4.classList.add('nc-1');
+    // elem4.classList.remove('nc-0', 'nc-2', 'nc-3'); 
+
+
+
+
+
+
+}
+function colorblue() {
+
+
+
+    document.body.style.backgroundColor = "#0A2B9F";
+    let elem1 = document.getElementById('hs');
+    elem1.classList = []
+    elem1.classList.add('hs-2');
+    let elem2 = document.getElementById('gr');
+    elem2.classList = []
+    elem2.classList.add('hs-2');
+    let elem3 = document.getElementById('cr');
+    elem3.classList = []
+    elem3.classList.add('hs-2');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList = []
+    elem4.classList.add('navelements', 'nc-2');
+    let elem5 = document.getElementById('txt');
+    elem5.classList = []
+    elem5.classList.add('txc');
+    let elem6 = document.getElementById('p5');
+    elem6.classList = []
+    elem6.classList.add('txc');
+    let elem7 = document.getElementById('ps');
+    elem7.classList = []
+    elem7.classList.add('txc');
+    let elem8 = document.getElementById('co');
+    elem8.classList = []
+    elem8.classList.add('txc');
+    let elem9 = document.getElementById('ct');
+    elem9.classList = []
+    elem9.classList.add('txc');
+    let elem10 = document.getElementById('sch');
+    elem10.classList = []
+    elem10.classList.add('searchbar', 'scb');
+    let elem11 = document.getElementById('bt1');
+    elem11.classList = []
+    elem11.classList.add('about', 'nc-2');
+    let elem12 = document.getElementById('bt2');
+    elem12.classList = []
+    elem12.classList.add('contact', 'bn2');
+
+    let elem13 = document.getElementById('join');
+    elem13.classList = []
+    elem13.classList.add('logo', 'nc-2');
+
+}
+function colorred() {
+
+
+
+    document.body.style.backgroundColor = "#B54242";
+    let elem1 = document.getElementById('hs');
+    elem1.classList = []
+    elem1.classList.add('hs-2');
+    let elem2 = document.getElementById('gr');
+    elem2.classList = []
+    elem2.classList.add('hs-2');
+    let elem3 = document.getElementById('cr');
+    elem3.classList = []
+    elem3.classList.add('hs-2');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList = []
+    elem4.classList.add('navelements', 'nc-3');
+    let elem5 = document.getElementById('txt');
+    elem5.classList = []
+    elem5.classList.add('txc');
+    let elem6 = document.getElementById('p5');
+    elem6.classList = []
+    elem6.classList.add('txc');
+    let elem7 = document.getElementById('ps');
+    elem7.classList = []
+    elem7.classList.add('txc');
+    let elem8 = document.getElementById('co');
+    elem8.classList = []
+    elem8.classList.add('txc');
+    let elem9 = document.getElementById('ct');
+    elem9.classList = []
+    elem9.classList.add('txc');
+    let elem10 = document.getElementById('sch');
+    elem10.classList = []
+    elem10.classList.add('searchbar', 'scb');
+    let elem11 = document.getElementById('bt1');
+    elem11.classList = []
+    elem11.classList.add('about', 'nc-3');
+    let elem12 = document.getElementById('bt2');
+    elem12.classList = []
+    elem12.classList.add('contact', 'bn2');
+
+    let elem13 = document.getElementById('join');
+    elem13.classList = []
+    elem13.classList.add('logo', 'nc-3');
+
+}
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
